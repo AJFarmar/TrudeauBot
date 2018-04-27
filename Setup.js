77b19632a11c2f48001678e0173dd2813ee9ff78
Anton Farmar <AJFarmar@gmail.com>
2018-04-27 20:19:13 +0100

    Setup.js now has a minimal setup script.

--- Setup.js
+++ Setup.js
@@ -1,1 +1,17 @@
+// A setup script for TrudeauBot
+// Usage: node Setup.js <IPA token>
+
+// Require the filesystem
+const fs = require('fs');
+const args = process.argv.slice(2);
+const token = args[0];
+
+fs.writeFile("./token.json", '{"token":"' + token + '"}\n', err => {
+    if (err) {
+        console.log("Encountered an error whilst writing the token.json file.");
+        console.log(err);
+    } else {
+        console.log("Wrote token.js successfully");
+    }
+})
 
